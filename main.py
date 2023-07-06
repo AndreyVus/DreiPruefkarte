b20f9ba6132c1021cec176823e1b35805d70af4b
AndreyVus <62697596+AndreyVus@users.noreply.github.com>
2023-07-06 16:16:10 +0200

    HTML Template für drei Prüfkarten als Vorlage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
-from pypdf import PdfReader, PdfWriter
+#from pypdf import PdfReader, PdfWriter
+#from fillpdf import fillpdfs
 
 data = [
 	[1, '06-07-2023 10:30', 'name1', '100% geprüft'],
@@ -8,34 +9,37 @@ data = [
 	[4, '06-07-2023 10:45', 'name4', '100% geprüft'],
 ]
 
-reader = PdfReader("form.pdf")
-writer = PdfWriter()
+# teilen data in 3er Gruppe
+data123 = [data[i:i+3] for i in range(0, len(data), 3)]
+# vorlage
+#reader = PdfReader("form.pdf")
+# ergebnis
+#writer = PdfWriter()
+# aktuelle Seitennummer
+pageNum = 0
+# füllen 3 Karten auf Seite aus
+for card123 in data123:
+	# add page
+	#writer.append(reader)
+	# card = dict(text1='', text2='', text3='', text4='',
+	#             text5='', text6='', text7='', text8='',
+	#             text9='', text10='', text11='', text12='')
+	# Stueckzahl, Datum, Name, Bemerkung   * 3
+	card = {}
+	count = 1
+	for row in card123:
+		for item in row:
+			key = f'text{count}'
+			card[key] = item
+			count += 1
+	print(card)
+	#writer.update_page_form_field_values(
+	#	writer.pages[pageNum], card
+	#)
+	#fillpdfs.write_fillable_pdf('form.pdf', f'new{pageNum}.pdf', card)
+	# next page
+	pageNum += 1
 
-'''
-def a3(reader, writer, Stueckzahl, Datum, Name, Bemerkung):
-
-	writer.append_pages_from_reader(reader)
-
-	writer.update_page_form_field_values(
-	    writer.pages[0], {"Stueckzahl": Stueckzahl}
-	)
-	writer.update_page_form_field_values(
-	    writer.pages[0], {"Datum": Datum}
-	)
-	writer.update_page_form_field_values(
-	    writer.pages[0], {"Name": Name}
-	)
-	writer.update_page_form_field_values(
-	    writer.pages[0], {"Bemerkung": Bemerkung}
-	)
-
-	# write "output" to PyPDF2-output.pdf
-	with open("filled-out.pdf", "wb") as output_stream:
-		writer.write(output_stream)
-'''
-
-for Stueckzahl, Datum, Name, Bemerkung in data[0:3]:
-	print(Stueckzahl, Datum, Name, Bemerkung)
-data = data[3:]
-for Stueckzahl, Datum, Name, Bemerkung in data[0:3]:
-	print(Stueckzahl, Datum, Name, Bemerkung)
+# write "output" to pypdf-output.pdf
+#with open("filled-out.pdf", "wb") as output_stream:
+#    writer.write(output_stream)
